bce518856e7be1bc3e808cfd9d06792985b2b78c
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-10-06 19:19:21 -0400

    part1

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The cat", "My grandpa", "Her turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "flies"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = ["before the class", "right on time", "when I finished", "during my lunch", "while I was praying",
+  ];
+
+  function randomElement(array) {
+    return array[Math.round(Math.random() * array.length)];
+  }
+  let excuse =
+    randomElement(who) +
+    " " +
+    randomElement(action) +
+    " " +
+    randomElement(what) +
+    " " +
+    randomElement(when) +
+    " " +
+    console.log("Hello Rigo from the console!");
+  console.log(excuse);
+ 
 };
