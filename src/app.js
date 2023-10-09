d459bb22bafb8000210a6ab9da16e86b1e9c4fdd
Gonzalo Espinel <gonzaloespinel@outlook.com>
2023-10-09 14:08:39 -0400

    fixes

--- src/app.js
+++ src/app.js
@@ -6,32 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["The cat", "My panda", "Her hippo", "My bird"];
-  let action = ["ate", "peed", "crushed", "flies"];
-  let what = ["my cake", "the keys", "the car"];
-  let when = [
-    "during class",
-    "outside",
-    "when I finished",
-    "during my lunch",
-    "while I was peeing"
-  ];
+    let who = ["The cat", "My panda", "The hippo", "My bird", "The Dog", "Fox"];
+    let action = ["ate", "peed", "crushed", "flies", "Pooped", "got drunk"];
+    let what = ["my cake", "the keys", "the car", "the boat", "the train"];
+    let when = [
+        "during class",
+        "while outside",
+        "when I finished eating",
+        "during my lunch hour",
+        "while I was peeing", "drinking beer"
+    ];
 
-  function randomElement(array) {
-    return array[Math.floor(Math.random() * array.length)];
-  }
-  let my_excuse =
-    randomElement(who) +
-    " " +
-    randomElement(action) +
-    " " +
-    randomElement(what) +
-    " " +
-    randomElement(when) +
-    " ";
-  console.log("Hello Tipo from the console!");
-  console.log(my_excuse);
-  const elementTheExcuse = document.getElementsByTagName("p");
-  elementTheExcuse[0].innerText = my_excuse;
+    function randomAnswer(array) {
+        return array[Math.floor(Math.random() * array.length)];
+    }
+    let my_excuse = randomAnswer(who).concat(randomAnswer(action), randomAnswer(what), randomAnswer(when));
+    console.log("Hello I'm the Tipo from the console!");
+    console.log(my_excuse);
+
+    const elementTheExcuse = document.getElementsByTagName("p");
+    elementTheExcuse[0].innerText = my_excuse;
 };
-
